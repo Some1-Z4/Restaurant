b788d030eed4fcbc7e93c6494d6513651b10ab46
zura patashuri <patashurizura@gmail.com>
2026-03-23 20:16:21 +0400

    s

--- login.js
+++ login.js
@@ -2,22 +2,31 @@ let phone = document.querySelector("#phone")
 let password = document.querySelector("#password")
 let register = document.querySelector("#register")
 
+register.addEventListener("click", async () => {
+    try {
+        const resp = await fetch("https://rentcar.stepprojects.ge/api/Users/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                phoneNumber: phone.value,
+                password: password.value
+            })
+        })
 
-register.addEventListener("click", ()=>{
-    fetch("https://rentcar.stepprojects.ge/api/Users/login", {
-     method : "POST",
-     headers : {
-        'Content-Type' : "application/json"
-     },
-     body : JSON.stringify({
-        phoneNumber : phone.value,
-        password : password.value
-     })
-    })
-    .then(resp => resp.json())
-    .then(data => {
+        if (!resp.ok) {
+            alert("User can't be found!")
+            return
+        }
+
+        const data = await resp.json()
         localStorage.setItem("token", data.token)
         localStorage.setItem("phone", phone.value)
         window.location.href = "./index.html"
-    })
+
+    } catch (err) {
+        alert("Something went wrong, please try again!")
+        console.error("Login error:", err)
+    }
 })
